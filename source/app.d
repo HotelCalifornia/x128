b731c14fa3ed80b5fd6b39e91b03587c164b232f
Alex Brooke <akb.sbc@gmail.com>
2018-10-11 13:55:47 -0400

    add sentiment analysis

--- source/app.d
+++ source/app.d
@@ -2,10 +2,16 @@ module app;
 
 import std.algorithm;
 import std.array;
-import std.format;
+import std.conv : to;
+import std.format : format;
+/* import std.socket : InternetAddress, Socket, SocketException, SocketSet, TcpSocket; */
+import std.regex;
 import std.stdio;
 
 import discord.w;
+
+import vibe.vibe;
+import vibe.data.json;
 
 alias cmd_fn_t = void delegate(Snowflake, string[]) @safe;
 
@@ -40,7 +46,7 @@ class X128 : DiscordGateway {
 		x128.channel(channel_id).sendMessage(msg);
 	}
 
-	override void onMessageCreate(Message m) @safe {
+	override void onMessageCreate(Message m) {
 		super.onMessageCreate(m);
 
 		// don't process messages from this bot
@@ -53,16 +59,31 @@ class X128 : DiscordGateway {
 			} else {
 				sendMessage(m.channel_id, format("Unknown command `%s`. Run `:> help` for a list of available commands", args[0]));
 			}
+		} else { // sentiment analysis
+			requestHTTP(format(`http://localhost:%s/`, nlpPort),
+				(scope HTTPClientRequest req) {
+					req.method = HTTPMethod.POST;
+					req.writeJsonBody(replaceAll(m.content, regex(r"[\U00010000-\U0010ffff]", "g"), ""));
+				},
+				(scope HTTPClientResponse res) {
+					auto json = res.readJson();
+					sendMessage(m.channel_id, json["sentences"].get!(Json[])[0]["sentiment"].get!string);
+				}
+			);
 		}
 	}
 }
 
 DiscordBot x128;
+string nlpPort = "9000";
 
 void main(string[] args) {
 	if (args.length <= 1) {
-		writeln("Usage: ", args[0], " [token]");
+		writeln("Usage: ", args[0], " token [NLP server port (default 9000)]");
 		return;
+	}
+	if (args.length > 2) {
+		nlpPort = args[2];
 	}
 	x128 = makeBot!X128(args[1]);
 
