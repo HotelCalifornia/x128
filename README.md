24892ddf64748ab62de192fbcc69e1f9debbf660
Alex Brooke <akb.sbc@gmail.com>
2018-10-11 02:12:39 -0400

    add initial bot framework

---
+++ README.md
@@ -0,0 +1,8 @@
+# x128
+
+big sister to HotelCalifornia/x64
+
+# requirements
+
+- all the stuff required to build a Dlang program ([https://dlang.org](check it out))
+- you may need to have libssl1.0-dev installed. apparently there's a different workaround but this one seems to work 🤷‍♀️ (vibe-d/vibe.d#1651)
--- source/app.d
+++ source/app.d
@@ -1,6 +1,72 @@
+module app;
+
+import std.algorithm;
+import std.array;
+import std.format;
 import std.stdio;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+import discord.w;
+
+alias cmd_fn_t = void delegate(Snowflake, string[]) @safe;
+
+class X128 : DiscordGateway {
+	struct Command {
+		cmd_fn_t fn;
+		string help_msg;
+	}
+
+	void ping(Snowflake channel_id, string[] args) @safe {
+		sendMessage(channel_id, "pong");
+	}
+	void help(Snowflake channel_id, string[] args) @safe {
+		string[] msg;
+		foreach (cmd; commands.byPair()) {
+			msg ~= format("`%s`: %s", cmd.key, cmd.value.help_msg);
+		}
+		// TODO: make fancy embed?
+		sendMessage(channel_id, msg.join("\n"));
+	}
+
+	Command[string] commands;
+	this(string token) {
+		super(token);
+		commands = [
+			"ping": Command(&ping, "replies with pong"),
+			"help": Command(&help, "prints this message")
+		];
+	}
+
+	void sendMessage(Snowflake channel_id, string msg) @safe {
+		x128.channel(channel_id).sendMessage(msg);
+	}
+
+	override void onMessageCreate(Message m) @safe {
+		super.onMessageCreate(m);
+
+		// don't process messages from this bot
+		if (m.author.id == this.info.user.id) return;
+
+		if (m.content.startsWith(":>")) { // hotword
+			auto args = m.content.split(" ")[1 .. $];
+			if ((args[0] in commands) !is null) { // valid command
+				commands[args[0]].fn(m.channel_id, args[1 .. $]);
+			} else {
+				sendMessage(m.channel_id, format("Unknown command `%s`. Run `:> help` for a list of available commands", args[0]));
+			}
+		}
+	}
+}
+
+DiscordBot x128;
+
+void main(string[] args) {
+	if (args.length <= 1) {
+		writeln("Usage: ", args[0], " [token]");
+		return;
+	}
+	x128 = makeBot!X128(args[1]);
+
+	while (x128.gateway.connected) {
+		sleep(10.msecs);
+	}
 }
