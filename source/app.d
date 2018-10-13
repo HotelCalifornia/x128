fc0c43dc8a925ca18de371424bfbb0f8c314a990
Alex Brooke <akb.sbc@gmail.com>
2018-10-12 20:17:11 -0400

    add formatting and documentation

--- source/app.d
+++ source/app.d
@@ -4,6 +4,7 @@ import std.algorithm;
 import std.array;
 import std.conv : to;
 import std.format : format;
+
 /* import std.socket : InternetAddress, Socket, SocketException, SocketSet, TcpSocket; */
 import std.regex;
 import std.stdio;
@@ -19,168 +20,265 @@ import vibe.data.serialization : name;
 import vibe.db.mongo.mongo;
 
 alias cmd_fn_t = void delegate(Snowflake, string[]) @safe;
-
+/**
+ * Main Bot class
+ */
 class X128 : DiscordGateway {
-	template WholesomeT() {
-		struct {
-			@name("_id") BsonObjectID id;
-			int neutral = 0;
-			int positive = 0;
-			int negative = 0;
-		}
-	}
-	struct WholesomeUser {
-		mixin WholesomeT;
+  /**
+  * Template containing common fields for database documents
+  */
+  template WholesomeT() {
+    struct {
+      @name("_id") BsonObjectID id;
+      int neutral;
+      int positive;
+      int negative;
+    }
+  }
 
-		struct Usr {
-			Snowflake uid;
-			string name;
-		}
-		Usr usr;
-	}
-	struct WholesomeChannel {
-		mixin WholesomeT;
+  /**
+   * Schema for user document in database
+   */
+  struct WholesomeUser {
+    mixin WholesomeT;
 
-		struct Chnl {
-			Snowflake cid;
-			string name;
-		}
-		Chnl chnl;
-	}
-	struct Command {
-		cmd_fn_t fn;
-		string help_msg;
-	}
+    private struct Usr {
+      Snowflake uid;
+      string name;
+    }
 
-	void ping(Snowflake channel_id, string[] args) @safe {
-		sendMessage(channel_id, "pong");
-	}
-	void help(Snowflake channel_id, string[] args) @safe {
-		string[] msg;
-		foreach (cmd; commands.byPair()) {
-			msg ~= format("`%s`: %s", cmd.key, cmd.value.help_msg);
-		}
-		// TODO: make fancy embed?
-		sendMessage(channel_id, msg.join("\n"));
-	}
-	void summary(Snowflake channel_id, string[] args) @safe {
-		auto channels = db.getCollection("local.channels");
-		auto channel = channels.findOne!WholesomeChannel(["chnl.cid": channel_id.toString()]);
-		if (channel.isNull) {
-			sendMessage(channel_id, "I don't have enough data about this channel yet!");
-		} else {
-			
-		}
-	}
+    Usr usr; /** Stores the user's ID and name */
+  }
 
-	Command[string] commands;
-	this(string token) {
-		super(token);
-		commands = [
-			"ping": Command(&ping, "replies with pong"),
-			"help": Command(&help, "prints this message")
-		];
-	}
+  /**
+   * Schema for channel document in database
+   */
+  struct WholesomeChannel {
+    mixin WholesomeT;
 
-	void sendMessage(Snowflake channel_id, string msg) @safe {
-		x128.channel(channel_id).sendMessage(msg);
-	}
+    private struct Chnl {
+      Snowflake cid;
+      string name;
+    }
 
-	Bson sentimentIncr(W)(string sentiment, W old) @safe {
-		Bson upd = Bson.emptyObject;
-		int newVal;
-		switch (sentiment) {
-			case "Neutral":
-				newVal = old.neutral + 1;
-				break;
-			case "Positive":
-				newVal = old.positive + 1;
-				break;
-			case "Negative":
-				newVal = old.negative + 1;
-				break;
-			default: assert(0); // should never ever happen
-		}
-		upd[sentiment.toLower()] = newVal;
-		return upd;
-	}
+    Chnl chnl; /** Stores the channel's ID and name */
+  }
 
-	void userSentiment(string sentiment, User author) @safe {
-		auto users = db.getCollection("local.users");
+  /**
+   * Representation of a bot command.
+   */
+  struct Command {
+    cmd_fn_t fn; /** command function */
+    string help_msg; /** short description of the command to display for the help message */
+    string usage; /** describe the usage of the command, e.g. `<: cmd arg1 [opt_arg2]` */
+  }
 
-		/* sendMessage(m.channel_id, json["sentences"].get!(Json[])[0]["sentiment"].get!string); */
-		auto user = users.findOne!WholesomeUser(["usr.uid": author.id.toString()]);
-		if (user.isNull) {
-			WholesomeUser usr = { id: BsonObjectID.generate(), usr: { uid: author.id, name: author.username } };
-			users.insert(usr);
-			user = users.findOne!WholesomeUser(["usr.uid": author.id.toString()]);
-		}
-		/* writeln(user.toString()); */
-		/* writeln(upd); */
-		users.update(["usr.uid": author.id.toString()], ["$set": sentimentIncr!WholesomeUser(sentiment, user)]);
-	}
+  /**
+    * Ping command
+    *
+    * Replies with 'pong'
+    *
+    * @param channel_id the ID of the channel in which to reply
+    */
+  void ping(Snowflake channel_id, string[] _) @safe {
+    sendMessage(channel_id, "pong");
+  }
 
-	void channelSentiment(string sentiment, User user, Channel chnl) @safe {
-		auto channels = db.getCollection("local.channels");
-		auto channel = channels.findOne!WholesomeChannel(["chnl.cid": chnl.id]);
-		if (channel.isNull) {
-			WholesomeChannel tchnl = { id: BsonObjectID.generate(), chnl: { cid: chnl.id, name: chnl.name } };
-			channels.insert(tchnl);
-			channel = channels.findOne!WholesomeChannel(["chnl.cid": chnl.id.toString()]);
-		}
+  /**
+   * Help command
+   *
+   * Lists all available commands and their help messages
+   *
+   * @param channel_id the ID of the channel in which to send the message
+   * @param args optionally specify the name of a command to get more detail
+   */
+  void help(Snowflake channel_id, string[] args) @safe {
+    if (args.length > 0) {
+      if (args[0] in commands)
+        sendMessage(channel_id, format("`%s`: %s\nUsage:\n```\n%s\n```",
+            args[0], commands[args[0]].help_msg, commands[args[0]].usage));
+      else
+        sendMessage(channel_id, format("No such command `%s`", args[0]));
 
-		channels.update(["chnl.cid": chnl.id.toString()], ["$set": sentimentIncr!WholesomeChannel(sentiment, channel)]);
-	}
+      return;
+    }
+    string[] msg;
+    foreach (cmd; commands.byPair()) {
+      msg ~= format("`%s`: %s", cmd.key, cmd.value.help_msg);
+    }
+    // TODO: make fancy embed?
+    sendMessage(channel_id, msg.join("\n"));
+  }
 
-	override void onMessageCreate(Message m) {
-		super.onMessageCreate(m);
+  /**
+   * Wholesomeness summary command
+   *
+   * Print out a summary detailing the overall wholesomeness of the channel or
+   * of a user
+   *
+   * @param channel_id the channel to check and in which send the message
+   * @param args
+   */
+  void summary(Snowflake channel_id, string[] args) @safe {
+    auto channels = db.getCollection("local.channels");
+    const auto channel = channels.findOne!WholesomeChannel(["chnl.cid" : channel_id.toString()]);
+    if (channel.isNull) {
+      sendMessage(channel_id, "I don't have enough data about this channel yet!");
+    }
+    else {
+      if (args.length == 0) { // channel summary
 
-		// don't process messages from this bot
-		if (m.author.id == this.info.user.id) return;
+      }
+    }
+  }
 
-		if (m.content.startsWith(":>")) { // hotword
-			auto args = m.content.split(" ")[1 .. $];
-			if ((args[0] in commands) !is null) { // valid command
-				commands[args[0]].fn(m.channel_id, args[1 .. $]);
-			} else {
-				sendMessage(m.channel_id, format("Unknown command `%s`. Run `:> help` for a list of available commands", args[0]));
-			}
-		} else { // sentiment analysis
-			requestHTTP(format(`http://localhost:%s/`, nlpPort),
-				(scope HTTPClientRequest req) {
-					req.method = HTTPMethod.POST;
-					req.writeJsonBody(replaceAll(m.content, regex(r"[\U00010000-\U0010ffff]", "g"), ""));
-				},
-				(scope HTTPClientResponse res) {
-					auto json = res.readJson();
-					auto users = db.getCollection("local.users");
+  Command[string] commands; /** map of command names -> commands */
+  /**
+    * Constructor
+    *
+    * @param token the bot's token
+    */
+  this(string token) {
+    super(token);
+    commands = ["ping" : Command(&ping, "replies with pong", ":> ping"), "help" : Command(&help,
+        "prints this message. if a command is specified, print a message describing its use",
+        ":> help [command]")];
+  }
 
-					sendMessage(m.channel_id, json["sentences"].get!(Json[])[0]["sentiment"].get!string);
-					auto sentiment = json["sentences"].get!(Json[])[0]["sentiment"].get!string;
-					userSentiment(sentiment, m.author);
-					channelSentiment(sentiment, m.author, x128.channel(m.channel_id).get());
-				}
-			);
-		}
-	}
+  /**
+    * Utility function for sending a message
+    *
+    * @param channel_id the channel in which to send the message
+    * @param msg the message to send
+    */
+  void sendMessage(Snowflake channel_id, string msg) @safe {
+    x128.channel(channel_id).sendMessage(msg);
+  }
+
+  /**
+    * Utility function for generating a mongodb update object
+    *
+    * @param sentiment the sentiment string returned from the NLP server
+    * @param old the record being updated
+    *
+    * @return an object that can be passed into a mongodb update function
+    */
+  Bson sentimentIncr(W)(string sentiment, W old) @safe {
+    Bson upd = Bson.emptyObject;
+    int newVal;
+    switch (sentiment) {
+    case "Neutral":
+      newVal = old.neutral + 1;
+      break;
+    case "Positive":
+      newVal = old.positive + 1;
+      break;
+    case "Negative":
+      newVal = old.negative + 1;
+      break;
+    default:
+      assert(0); // should never ever happen
+    }
+    upd[sentiment.toLower()] = newVal;
+    return upd;
+  }
+
+  /**
+    * Push a new user with sentiment values or update an existing one
+    *
+    * @param sentiment the sentiment string returned by the NLP server
+    * @param author the author of the message that was just analyzed
+    */
+  void userSentiment(string sentiment, User author) @safe {
+    const auto users = db.getCollection("local.users");
+
+    auto user = users.findOne!WholesomeUser(["usr.uid" : author.id.toString()]);
+    if (user.isNull) {
+      WholesomeUser usr = {
+      id:
+        BsonObjectID.generate(), usr : {uid:
+        author.id, name : author.username}
+      };
+      users.insert(usr);
+      user = users.findOne!WholesomeUser(["usr.uid" : author.id.toString()]);
+    }
+    users.update(["usr.uid" : author.id.toString()], ["$set"
+        : sentimentIncr!WholesomeUser(sentiment, user)]);
+  }
+
+  /**
+    * Push a new channel with sentiment values or update an existing one
+    *
+    * @param sentiment the sentiment string returned by the NLP server
+    * @param user TBD
+    * @param chnl the channel to insert or update
+    */
+  void channelSentiment(string sentiment, User user, Channel chnl) @safe {
+    auto channels = db.getCollection("local.channels");
+    auto channel = channels.findOne!WholesomeChannel(["chnl.cid" : chnl.id]);
+    if (channel.isNull) {
+      WholesomeChannel tchnl = {
+      id:
+        BsonObjectID.generate(), chnl : {cid:
+        chnl.id, name : chnl.name}
+      };
+      channels.insert(tchnl);
+      channel = channels.findOne!WholesomeChannel(["chnl.cid" : chnl.id.toString()]);
+    }
+
+    channels.update(["chnl.cid" : chnl.id.toString()], ["$set"
+        : sentimentIncr!WholesomeChannel(sentiment, channel)]);
+  }
+
+  override void onMessageCreate(Message m) {
+    super.onMessageCreate(m);
+
+    // don't process messages from this bot
+    if (m.author.id == this.info.user.id)
+      return;
+
+    if (m.content.startsWith(":>")) { // hotword
+      auto args = m.content.split(" ")[1 .. $];
+      if ((args[0] in commands) !is null) { // valid command
+        commands[args[0]].fn(m.channel_id, args[1 .. $]);
+      }
+      else {
+        sendMessage(m.channel_id,
+            format("Unknown command `%s`. Run `:> help` for a list of available commands", args[0]));
+      }
+    }
+    else { // sentiment analysis
+      requestHTTP(format(`http://localhost:%s/`, nlpPort), (scope HTTPClientRequest req) {
+        req.method = HTTPMethod.POST;
+        req.writeJsonBody(replaceAll(m.content, regex(r"[\U00010000-\U0010ffff]", "g"), ""));
+      }, (scope HTTPClientResponse res) {
+        auto json = res.readJson();
+
+        sendMessage(m.channel_id, json["sentences"].get!(Json[])[0]["sentiment"].get!string);
+        auto sentiment = json["sentences"].get!(Json[])[0]["sentiment"].get!string;
+        userSentiment(sentiment, m.author);
+        channelSentiment(sentiment, m.author, x128.channel(m.channel_id).get());
+      });
+    }
+  }
 }
 
-DiscordBot x128;
-string nlpPort = "9000";
-MongoClient db;
+DiscordBot x128; /** main bot instance */
+string nlpPort = "9000"; /** port on which the NLP server is running. can be overridden by command line arguments */
+MongoClient db; /** mongo database instance */
 
 void main(string[] args) {
-	if (args.length <= 1) {
-		writeln("Usage: ", args[0], " token [NLP server port (default 9000)]");
-		return;
-	}
-	if (args.length > 2) {
-		nlpPort = args[2];
-	}
-	db = connectMongoDB("127.0.0.1");
-	x128 = makeBot!X128(args[1]);
+  if (args.length <= 1) {
+    writeln("Usage: ", args[0], " token [NLP server port (default 9000)]");
+    return;
+  }
+  if (args.length > 2) {
+    nlpPort = args[2];
+  }
+  db = connectMongoDB("127.0.0.1");
+  x128 = makeBot!X128(args[1]);
 
-	while (x128.gateway.connected) {
-		sleep(10.msecs);
-	}
+  while (x128.gateway.connected) {
+    sleep(10.msecs);
+  }
 }
